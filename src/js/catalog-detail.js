66f819c048419fef3e14931bf8496018a73753e8
unknown <redison.ray3318@gmail.com>
2026-02-12 02:49:21 +0300

    Feature: Добавлены страницы "Все проекты", "Каталог домов" и "Детальная страница дома"

--- src/js/catalog-detail.js
+++ src/js/catalog-detail.js
@@ -4,7 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
     slidesPerView: 'auto',
     freeMode: true,
     watchSlidesProgress: true,
-    breakpoints: {},
+    breakpoints: {
+      320: {
+        spaceBetween: 12,
+      },
+      960: {
+        spaceBetween: 16,
+      },
+    },
   });
 
   const swiper2 = new Swiper('.catalog-detail__slider', {
@@ -25,6 +32,14 @@ document.addEventListener('DOMContentLoaded', () => {
     slidesToScroll: 1,
     nextArrow: createArrow('next'),
     prevArrow: createArrow('prev'),
+    responsive: [
+      {
+        breakpoint: 581,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 
   function createArrow(direction) {
@@ -34,5 +49,61 @@ document.addEventListener('DOMContentLoaded', () => {
         : '<svg width="7" height="15" viewBox="0 0 7 15" fill="none"><path d="M6.13672 14.3154L0.636719 7.09321L6.13672 0.31543" stroke="black"/></svg>';
 
     return `<button type="button" class="slick-${direction}" aria-label="${direction === 'next' ? 'Следующий' : 'Предыдущий'}">${svg}</button>`;
+  }
+
+  if (document.querySelector('.equipment')) {
+    const services = document.querySelector('.equipment');
+    const tabs = services.querySelector('.equipment__buttons');
+    const imgs = services.querySelector('.equipment__wrap');
+
+    for (const [index, tab] of tabs.querySelectorAll('button').entries()) {
+      tab.addEventListener('click', () => {
+        for (const ostTab of tabs.querySelectorAll('button')) {
+          ostTab.classList.remove('tab__button--active');
+        }
+
+        for (const img of imgs.querySelectorAll('.tab')) {
+          img.classList.remove('tab--active');
+        }
+
+        tab.classList.add('tab__button--active');
+        imgs.querySelectorAll('.tab')[index].classList.add('tab--active');
+      });
+    }
+  }
+
+  if (document.querySelector('.gift')) {
+    const achievement = document.querySelector('.gift');
+    const cards = achievement.querySelectorAll('.gift__card');
+
+    let currentIndex = 0;
+
+    cards[currentIndex].classList.add('card--active');
+    setInterval(() => {
+      cards[currentIndex].classList.remove('card--active');
+      currentIndex = (currentIndex + 1) % cards.length;
+      cards[currentIndex].classList.add('card--active');
+    }, 5000);
+  }
+
+  if (document.querySelector('.design-swiper')) {
+    const swiper3 = new Swiper('.design-swiper', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      loop: true,
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 100,
+        depth: 150,
+        modifier: 1.2,
+        slideShadows: false,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
   }
 });
--- src/js/catalog.js
+++ src/js/catalog.js
@@ -25,4 +25,70 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     }
   }
+
+  const rangeSlider = document.getElementById('rangeSlider');
+  if (rangeSlider) {
+    noUiSlider.create(rangeSlider, {
+      start: [50, 500],
+      margin: 10,
+      connect: true,
+      step: 1,
+      range: {
+        min: [0],
+        max: [560],
+      },
+    });
+  }
+
+  const input1 = document.getElementById('range-input-1');
+  const input2 = document.getElementById('range-input-2');
+  const inputs = [input1, input2];
+
+  rangeSlider.noUiSlider.on('update', function (values, handle) {
+    inputs[handle].value = Math.round(values[handle]);
+  });
+  const setRangeSlider = (i, value) => {
+    let arr = [null, null];
+    arr[i] = value;
+    rangeSlider.noUiSlider.set(arr);
+  };
+  inputs.forEach((el, index) => {
+    el.addEventListener('change', (e) => {
+      setRangeSlider(index, e.currentTarget.value);
+    });
+  });
+
+  if (document.querySelector('.filter-list')) {
+    if (document.querySelector('.filter-list active'))
+      document.querySelector('.filter-list button').textContent = document.querySelector('.filter-list active').textContent;
+
+    document.querySelector('.filter-list button').addEventListener('click', () => {
+      if (document.querySelector('.filter-list__content').classList.contains('filter-list__content--active')) {
+        document.querySelector('.filter-list__content').classList.remove('filter-list__content--active');
+      } else {
+        document.querySelector('.filter-list__content').classList.add('filter-list__content--active');
+      }
+    });
+  }
+
+  document.querySelector('.filter-open').addEventListener('click', () => {
+    document.querySelector('.catalog__filter').classList.add('catalog__filter--active');
+    document.body.style.overflow = 'hidden';
+    const overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+
+    overlay.addEventListener('click', () => {
+      document.querySelector('.catalog__filter').classList.remove('catalog__filter--active');
+      document.body.style.overflow = 'visible';
+      overlay.remove();
+    });
+
+    document.body.appendChild(overlay);
+  });
+
+  document.querySelector('.filter-close').addEventListener('click', () => {
+    document.querySelector('.catalog__filter').classList.remove('catalog__filter--active');
+    document.body.style.overflow = 'visible';
+    document.querySelector('.overlay').remove();
+  });
 });
